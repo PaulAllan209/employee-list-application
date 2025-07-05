a43b833a818c46d5d521419ee0412960b0b3a61c
PaulAllan209 <dpaulallan14@gmail.com>
2025-07-05 20:30:19 +0800

    added base for POST request and the EmployeeForCreationDto

--- EmployeeListApplication.Server/Controllers/EmployeeController.cs
+++ EmployeeListApplication.Server/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeListApplication.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeListApplication.Server.Controllers
@@ -7,5 +8,17 @@ namespace EmployeeListApplication.Server.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
