9b34d174f1f9254475f548b3b5a76c463ba42e56
PaulAllan209 <dpaulallan14@gmail.com>
2025-07-06 16:45:51 +0800

    finish connecting UI to CreateEmployees endpoint in backend

--- EmployeeListApplication.client/src/app/components/employee-list/employee-list.component.ts
+++ EmployeeListApplication.client/src/app/components/employee-list/employee-list.component.ts
@@ -6,6 +6,7 @@ import { ButtonModule } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Employee } from '../../models/employee.model';
 import { EmployeeService } from '../../services/employee.service';
 import { ApiResponse } from '../../models/api-response.model';
@@ -13,7 +14,7 @@ import { ApiResponse } from '../../models/api-response.model';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [TableModule, CommonModule, ButtonModule, Dialog, InputTextModule],
+  imports: [TableModule, CommonModule, ButtonModule, Dialog, InputTextModule, FormsModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.scss'
 })
@@ -24,12 +25,41 @@ export class EmployeeListComponent {
 
   constructor(private employeeService: EmployeeService) {}
 
+  newEmployee: Employee = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    position: ''
+  };
+
   showCreateDialog() {
+    // This ensures whenever we create new employee the newEmployee object is empty
+    this.newEmployee = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+      position: ''
+    };
+
     this.createEmployeeVisible = true;
   }
 
   ngOnInit(){
     this.fetchEmployees();
+  }
+
+  saveEmployee() {
+    this.employeeService.createEmployee(this.newEmployee).subscribe({
+      next: (createdEmployee) => {
+        this.createEmployeeVisible = false;
+        this.fetchEmployees(); // refresh the list
+      },
+      error: (err) => {
+        console.error('Error creating employee', err);
+      }
+    });
   }
 
   fetchEmployees(): void {
@@ -41,6 +71,17 @@ export class EmployeeListComponent {
       error: (err) => {
         console.error('Error fetching employees', err);
         this.loading = false;
+      }
+    });
+  }
+
+  deleteEmployee(id: string) {
+    this.employeeService.deleteEmployee(id).subscribe({
+      next: () => {
+        this.employees = this.employees.filter(emp => emp.id !== id);
+      },
+      error: (err) => {
+        console.error('Error deleting employee', err);
       }
     });
   }
