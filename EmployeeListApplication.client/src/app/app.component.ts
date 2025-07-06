32d70f41637fbdf1729060b131143c9a67f24127
PaulAllan209 <dpaulallan14@gmail.com>
2025-07-06 13:40:57 +0800

    added components

--- EmployeeListApplication.client/src/app/app.component.ts
+++ EmployeeListApplication.client/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
